a0156b328e46fdfef8dad9d563b09596e9edac77
rlaughlin22 <47514076+elaughlin22@users.noreply.github.com>
2021-08-24 14:36:46 -0400

    Move updates out of docs/

--- js/classroom.js
+++ js/classroom.js
@@ -1,4 +1,5 @@
 function getCourses() {
+<<<<<<< Updated upstream
 	const courseList = gapi.client.classroom.courses.list({
 		'courseStates': 'ACTIVE',
 		'studentId': 'me'
@@ -6,10 +7,46 @@ function getCourses() {
 	return courseList
 	.then(function (response) {
 		console.log(response.result.courses);
+=======
+  startLockout();
+	return gapi.client.classroom.courses.list({
+		"courseStates": "ACTIVE",
+		"studentId": "me"
+	}).then(function (response) {
+		createCourses(response);
+>>>>>>> Stashed changes
 	}).catch(function (reason) {
 		console.log(reason);
 	});
 }
 
 
+<<<<<<< Updated upstream
 // getCourses().courses[x].name
+=======
+function createCourses(response) {
+  const courses = response.result.courses;
+  
+  for (let i = 0; i < courses.length; i++) {
+    if (courses[i].section !== undefined) {
+      let info = courses[i].section.match(classroomFormat);
+      if (info !== null) {
+        //try {
+          input[letterToNum(info.groups.period)].value = courses[i].name;
+          radio[letterToNum(info.groups.period)][parseInt(info.groups.lunch, 10)-1].checked = true;
+          input[letterToNum(info.groups.period)+8].value = courses[i].name;
+          radio[letterToNum(info.groups.period)+8][parseInt(info.groups.lunch, 10)-1].checked = true;
+        /*} catch(err) {
+          console.log("Possibly incorrect information from Classroom");
+        }*/
+      }
+    }
+  }
+  endLockout();
+}
+
+
+function letterToNum(letter) {
+  return letter.charCodeAt(0) - 65;
+}
+>>>>>>> Stashed changes
--- js/handle-api.js
+++ js/handle-api.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // https://developers.google.com/calendar/v3/reference/events/insert#javascript
 // https://github.com/google/google-api-javascript-client/blob/master/docs
 // namespace may be needed
@@ -7,10 +8,39 @@ const API_KEY = 'AIzaSyAHzxZJfpp-RSjIVLAPy1TH8-4xAraOhu8';
 const CLIENT_ID = '1061840141453-3gdbhsibjbfgg4tkocqsuu9uv39adkcq.apps.googleusercontent.com';
 const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest', 'https://www.googleapis.com/discovery/v1/apis/classroom/v1/rest'];
 const SCOPES = 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/classroom.courses.readonly';
+=======
+var errorPage = document.getElementById("error");
+
+function handleClientLoad() {
+  try{
+    gapi.load("client:auth2", start);
+  } catch(err) {
+  }
+}
+>>>>>>> Stashed changes
 
 var authorizeButton = document.getElementById('authorize-button');
 var signoutButton = document.getElementById('signout-button');
 
+<<<<<<< Updated upstream
+=======
+// need to be web application with origin url added
+// need to setup localhost
+// enable apis, setup request screen
+const CLIENT_ID = "22227420304-65mol9e9a34j1c0an8vc1l0mab6m91sr.apps.googleusercontent.com";
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/classroom/v1/rest", "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest", "https://sheets.googleapis.com/$discovery/rest?version=v4"];
+const SCOPES = "https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/spreadsheets.readonly";
+
+var authorizeButton = document.getElementById("authorize-button");
+var signoutButton = document.getElementById("signout-button");
+var executeButton = document.getElementById("execute-button");
+var fetchButton = document.getElementById("fetch-button");
+
+var page1 = document.getElementById("page1");
+var page2 = document.getElementById("page2");
+var lockout = document.getElementById("lockout");
+
+>>>>>>> Stashed changes
 
 gapi.load('client:auth2', start);
 
@@ -29,12 +59,14 @@ function start() {
 
   }).catch(function (reason) {
     console.log(reason);
+    errorPage.style.display = "block";
   });
 };
 
 
 function updateSigninStatus(isSignedIn) {
   if (isSignedIn) {
+<<<<<<< Updated upstream
     authorizeButton.style.display = 'none';
     signoutButton.style.display = 'block';
 
@@ -42,6 +74,16 @@ function updateSigninStatus(isSignedIn) {
   } else {
     authorizeButton.style.display = 'block';
     signoutButton.style.display = 'none';
+=======
+    page1.style.display = "none";
+    page2.style.display = "block";
+    signoutButton.style.display = "block";
+
+  } else {
+    page1.style.display = "block";
+    page2.style.display = "none";
+    signoutButton.style.display = "none";
+>>>>>>> Stashed changes
   }
 }
 
